80d00d3306065d123a69cc5acafef1325180d359
nadaese <na95@kumoh.ac.kr>
2023-11-30 19:07:37 +0900

    수정중 pm 7:10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
 
 dependencies {
 
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
